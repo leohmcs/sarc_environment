4bde2046e11b77344b6cfdf60c7f3ec738a42386
leohmcs <leohmcs@gmail.com>
2022-10-13 13:17:57 -0300

    Simulate 4 drones.

--- scripts/fall.py
+++ scripts/fall.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import sys
+
 import rospy
 import rosnode
 import math
@@ -10,11 +12,25 @@ from std_srvs.srv import Trigger as Trigger
 from gazebo_msgs.srv import DeleteModel, DeleteModelRequest, ApplyBodyWrench
 from geometry_msgs.msg import Point, Wrench, Vector3
 
-global uav_name
-uav_name = "uav1"
+# global uav_name
+# uav_name = "uav1"
 class Activator:
+    def __init__(self, uav_id: int):
+        self.uav_name = "uav" + str(uav_id)
+
+        rospy.init_node('activator', anonymous=True) 
+
+        self.delete = rospy.ServiceProxy('/gazebo/delete_model', DeleteModel)    
+        self.subscriber = rospy.Subscriber('/' + self.uav_name + '/uav_manager/diagnostics', UavManagerDiagnostics, self.callback)
+        self.arm = rospy.ServiceProxy('/' + self.uav_name + '/mavros/cmd/arming', CommandBool)
+        self.activate = rospy.ServiceProxy('/' + self.uav_name + '/uav_manager/midair_activation', Trigger)
+        self.apply_wrench = rospy.ServiceProxy('/gazebo/apply_body_wrench', ApplyBodyWrench)
+        
+        rospy.loginfo('initialized')
+        rospy.spin()
+
     def callback(self, data):
-        body_name = uav_name + '::base_link'
+        body_name = self.uav_name + '::base_link'
         wrench = Wrench()
         force = [2, 2, 2]
         wrench.force = Vector3(*force)
@@ -32,21 +48,9 @@ class Activator:
         self.apply_wrench(body_name, 'world', Point(0, 0, 0), wrench, rospy.Time().now(), duration)
         rospy.signal_shutdown("yes")
 
-    def __init__(self):
-
-        rospy.init_node('activator', anonymous=True) 
-
-        self.delete = rospy.ServiceProxy('/gazebo/delete_model', DeleteModel)    
-        self.subscriber = rospy.Subscriber('/' + uav_name + '/uav_manager/diagnostics', UavManagerDiagnostics, self.callback)
-        self.arm = rospy.ServiceProxy('/' + uav_name + '/mavros/cmd/arming', CommandBool)
-        self.activate = rospy.ServiceProxy('/' + uav_name + '/uav_manager/midair_activation', Trigger)
-        self.apply_wrench = rospy.ServiceProxy('/gazebo/apply_body_wrench', ApplyBodyWrench)
-        
-        rospy.loginfo('initialized')
-        rospy.spin()
-
 if __name__ == '__main__':
     try:
-        node_crash_checker = Activator()
+        uav_id = int(sys.argv[1])
+        node_crash_checker = Activator(uav_id)
     except rospy.ROSInterruptException:
         pass
